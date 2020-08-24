19bbcc34184b2ae24dc2ba8815e00d0f7f76a86e
Jerrad Moon <jerradm85@gmail.com>
2020-08-24 11:54:29 -0700

    updated express boilerplate packages

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,9 +1,11 @@
+const knex = require('knex')
 const app = require('../src/app')
+const helpers = require('./test-helpers')
 
 describe('App', () => {
-  it('GET / responds with 200 containing "Hello, world!"', () => {
+  it('GET / responds with 200 containing "Express Boilerplate!"', () => {
     return supertest(app)
       .get('/')
-      .expect(200, 'Hello, boilerplate!')
+      .expect(200, 'Express Boilerplate!')
   })
 })
