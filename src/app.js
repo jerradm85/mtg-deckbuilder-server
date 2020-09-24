a87567558f2b7ef9bc562c11f29c9abf209c4831
Jerrad Moon <jerradm85@gmail.com>
2020-09-23 19:37:40 -0700

    mvp done

--- src/app.js
+++ src/app.js
@@ -28,10 +28,10 @@ app.use('/api/deckcards', deckCardsRouter)
 
 app.use(function errorHandler(error, req, res, next) {
     let response
+    console.error(error)
     if (NODE_ENV === 'production') {
         response = { error: { message: 'server error' } }
     } else {
-        console.error(error)
         response = { message: error.message, error }
     }
     res.status(500).json(response)
